f7c910dc54fbaccb3059002ab12544545558bb6a
Mishin Alexey <mishin-lex@mail.ru>
2020-08-30 17:08:28 +0300

    Add sendHttpRequest func

---
+++ core/src/utils/http/sendHttpRequest.ts
@@ -0,0 +1,23 @@
+import * as fetch from "isomorphic-fetch";
+
+interface ISendRequest extends RequestInit {
+    url: string;
+    data?: Record<string, unknown>;
+    headers?: HeadersInit;
+}
+
+export const sendHttpRequest = function sendHttpRequest({
+    url,
+    data,
+    headers,
+    ...options
+}: ISendRequest): Promise<Response> {
+    return fetch(url, {
+        body: data ? JSON.stringify(data) : undefined,
+        ...options as RequestInit,
+        headers: {
+            "Content-Type": "application/json",
+            ...headers,
+        },
+    });
+};
