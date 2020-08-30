1ff4db06faa447ea52397beee87284e91e4af753
Mishin Alexey <mishin-lex@mail.ru>
2020-08-30 17:11:44 +0300

    Add telegram sendMessage func

---
+++ core/src/externalServices/telegram/telegram.ts
@@ -0,0 +1,18 @@
+import { sendHttpRequest } from "../../utils/http/sendHttpRequest";
+
+const MAX_MESSAGE_LENGTH = 4096;
+
+export const sendMessage = async (
+    chatId: string,
+    botToken: string,
+    message: string
+): Promise<Response> => {
+    return sendHttpRequest({
+        url: `https://api.telegram.org/bot${botToken}/sendMessage`,
+        method: "POST",
+        data: {
+            chat_id: chatId,
+            text: message.substring(0, MAX_MESSAGE_LENGTH),
+        },
+    });
+};
